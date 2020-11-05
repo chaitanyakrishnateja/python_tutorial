9030bb11fab22c4bbb9b0f69921e6ebdda250d1a
chaitanyakrishnateja <67891378+chaitanyakrishnateja@users.noreply.github.com>
2020-11-05 11:58:04 +0530

    Add files via upload

---
+++ tutorial1.py
@@ -0,0 +1,11 @@
+
+name = input('what is your name? ')
+print("Hi " + name)
+color = input('what is your fav.color? ')
+print(name  + " likes " + color)
+Weight = input("what is your weight(in pounds)? ")
+Weight = float(Weight) * 0.453592
+print(type(Weight))
+weight = int(Weight)
+print(type(weight))
+print( weight )
